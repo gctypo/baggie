c5097e01511994ee33d7e156e1e31df537c00543
GCTypo <gctypo@outlook.com>
2023-07-05 18:42:40 -0700

    add basic console program

---
+++ Baggie/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.IO
+open Microsoft.Extensions.Configuration
+
+// Following: https://brandewinder.com/2021/10/30/fsharp-discord-bot/
+
+let appConfig =
+    ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("AppSettings.json", true, true)
+        .Build()
+
+[<EntryPoint>]
+let main argv =
+    let token = appConfig.["discord.token"]
+    printfn "Hello world"
+    printfn $"Token: %s{token}"
+
+    0
