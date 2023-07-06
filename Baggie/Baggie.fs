0d1c900c44b19fb83268861e592113cdfd85ee1b
GCTypo <gctypo@outlook.com>
2023-07-05 22:48:12 -0700

    add baggie copypasta command
    NOTE: OAuth2 permissions required:
    "Read Messages/View Channels", "Send Messages",
    "Read Message History" (68608)

--- Baggie/Baggie.fs
+++ Baggie/Baggie.fs
@@ -1,8 +1,45 @@
-module Baggie
+namespace Baggie
 
-// I'm not sorry.
-let BAGGIE = "D'awwww Baggie Waggie being all UwU and OwO on chat? \
-    D'awwww wook he's so embawassed uWu Does Baggie Waggie need a \
-    huggie wuggie poo? What about a kith? Baggie Waggie wan a kith? \
-    UwU ehe te nandayo~"
+open System.Threading.Tasks
 
+open DSharpPlus.CommandsNext
+open DSharpPlus.CommandsNext.Attributes
+open DSharpPlus.Entities
+
+type BaggieBot () =
+
+    inherit BaseCommandModule ()
+
+    // I'm not sorry.
+    let PASTA = "*D'awwww Baggie Waggie being all UwU and OwO on chat? \
+        D'awwww wook he's so embawassed uWu Does Baggie Waggie need a \
+        huggie wuggie poo? What about a kith? Baggie Waggie wan a kith? \
+        UwU ehe te nandayo~*"
+
+    [<Command "baggie">]
+    let baggie (ctx: CommandContext) =
+        task {
+            do!
+                ctx.TriggerTypingAsync()
+
+            let! _ =
+                PASTA
+                |> ctx.RespondAsync
+
+            return ()
+        }
+        :> Task
+
+    [<Command "baggie">]
+    let baggie (ctx: CommandContext, user: DiscordMember) =
+        task {
+            do!
+                ctx.TriggerTypingAsync()
+
+            let! _ =
+                $"%s{user.Mention} %s{PASTA}"
+                |> ctx.RespondAsync
+
+            return ()
+        }
+        :> Task
--- Baggie/Program.fs
+++ Baggie/Program.fs
@@ -1,24 +1,51 @@
-﻿open System.IO
-open DSharpPlus
-open Microsoft.Extensions.Configuration
+﻿namespace Baggie
+
+open Microsoft.Extensions.Logging
 
 // Following: https://brandewinder.com/2021/10/30/fsharp-discord-bot/
+module Program =
 
-let appConfig =
-    ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("AppSettings.json", true, true)
-        .Build()
+    open System.Threading.Tasks
+    open System.IO
+    open Microsoft.Extensions.Configuration
+    open DSharpPlus
+    open DSharpPlus.CommandsNext
 
-[<EntryPoint>]
-let main argv =
-    let token = appConfig.["discord.token"]
-    printfn "Hello world"
+    let appConfig =
+        ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("AppSettings.json", true, true)
+            .Build()
 
-    let config = DiscordConfiguration()
-    config.Token <- token
-    config.TokenType <- TokenType.Bot
+    [<EntryPoint>]
+    let main argv =
+        let token = appConfig.["discord.token"]
+        printfn "Hello world"
 
-    let client = new DiscordClient(config)
+        let config = DiscordConfiguration ()
+        config.Token <- token
+        config.MinimumLogLevel <- LogLevel.Debug
+        config.TokenType <- TokenType.Bot
+        config.Intents <- DiscordIntents.GuildMessages
+            ||| DiscordIntents.Guilds
+            ||| DiscordIntents.MessageContents
 
-    0
+        let discord = new DiscordClient(config)
+
+        let commandsConfig = CommandsNextConfiguration ()
+        commandsConfig.StringPrefixes <- ["!"; "/"]
+
+        let commands = discord.UseCommandsNext(commandsConfig)
+        commands.RegisterCommands<BaggieBot>()
+
+        printfn "Connecting to Discord"
+        discord.ConnectAsync()
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+        printfn "Ready."
+        Task.Delay(-1)
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
+
+        1
